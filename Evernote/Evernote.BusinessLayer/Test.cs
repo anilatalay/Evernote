08566817c2ff0a4f1783925ed6a093c62bd16672
Anıl Atalay <anilatalay@hotmail.com.tr>
2017-09-26 22:03:45 +0300

    Database Tabloları İçin Test Eklendi.

---
+++ Evernote/Evernote.BusinessLayer/Test.cs
@@ -0,0 +1,13 @@
+﻿using Evernote.DataAccessLayer;
+
+namespace Evernote.BusinessLayer
+{
+    public class Test
+    {
+        public Test()
+        {
+            var db = new DatabaseContext();
+            db.Database.CreateIfNotExists();
+        }
+    }
+}
--- Evernote/Evernote.Web/Controllers/HomeController.cs
+++ Evernote/Evernote.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Evernote.Web.Controllers
 {
@@ -11,6 +7,8 @@ namespace Evernote.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+
+            
             return View();
         }
     }
